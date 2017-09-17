315ded263d357500783a788ec250ed15b885d215
Gokberk Karaca <gskaraca1905@gmail.com>
2017-09-17 20:29:25 +0200

    Implement Exercise 3 - Counting Change

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -39,5 +39,15 @@ object Main {
   /**
    * Exercise 3
    */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+    def countChange(money: Int, coins: List[Int]): Int = {
+
+      if (money == 0)
+        1
+      else (
+        if (money < 0 || coins.isEmpty)
+          0
+        else
+          countChange(money - coins.head, coins) + countChange(money, coins.tail)
+        )
+    }
   }
